457d0fae4d5d80ee075ce11c1942ca729b70a6ea
fabioaananta <46307607+fabioaananta@users.noreply.github.com>
2019-01-02 10:27:49 +0700

    First website upload

---
+++ procces.php
@@ -0,0 +1,18 @@
+<html>
+	<head>
+		<title>form2</title>
+	</head>
+<body align="center">
+	<h3>Data Pendaftaran</h3>
+	
+	<?php
+		echo "Nama : ".$_POST['nama']."</br>";
+		echo "Password : ".$_POST['password']."</br>";
+		echo "Jenis Kelamin : ".$_POST['Jenis_kelamin']."</br>";
+		echo "Keahlian : ".$_POST['keahlian']."</br>";
+		echo "Tempat Lahir : ".$_POST['tempat_lahir']."</br>";
+		echo "Deskripsi : ".$_POST['deskripsi']."</br>";
+		
+	?>
+</body>
+</html>	
